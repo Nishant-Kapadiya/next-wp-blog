99f681947094cd8fd785d259fb7803fc1863f718
Nishant-Kapadiya <nishantkapadiya8@gmail.com>
2023-06-24 21:21:08 +0530

    Update footer

--- next.config.js
+++ next.config.js
@@ -6,14 +6,25 @@ if (!process.env.WORDPRESS_API_URL) {
 }
 
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   images: {
+    reactStrictMode: true,
     domains: [
+      'localhost',
       process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
       '0.gravatar.com',
       '1.gravatar.com',
       '2.gravatar.com',
       'secure.gravatar.com',
     ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dev-nishant-kapadiya.pantheonsite.io',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
   },
 }
+module.exports = nextConfig
